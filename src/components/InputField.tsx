4d78dcd124d67cc67eb64089de791de7000f31d4
Tanmay Makode <tanmay.s.makode@gmail.com>
2023-09-10 16:13:16 +0530

    added Todo App

---
+++ src/components/InputField.tsx
@@ -0,0 +1,38 @@
+import "./styles.css";
+
+import React, { useRef } from "react";
+
+interface Props {
+  todo: string;
+  setTodo: React.Dispatch<React.SetStateAction<string>>;
+  handleAdd: (e: React.FormEvent) => void;
+}
+
+// const InputField = ({todo, setTodo}: Props) => {
+const InputField: React.FC<Props> = ({ todo, setTodo, handleAdd }) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  return (
+    <form
+      className="input"
+      onSubmit={(e) => {
+        handleAdd(e);
+        inputRef.current?.blur();
+      }}
+    >
+      <input
+        ref={inputRef}
+        type="input"
+        value={todo}
+        onChange={(e) => setTodo(e.target.value)}
+        placeholder="Enter a task!"
+        className="input_box"
+      ></input>
+      <button className="input_submit" type="submit">
+        Add
+      </button>
+    </form>
+  );
+};
+
+export default InputField;
